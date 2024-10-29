fcab48a9061d3a215e79fa4e6d3913d531664775
litury <litury@icloud.com>
2024-10-30 04:49:41 +0500

    ✨ feat(client): Добавлен роутер в приложение

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,9 +1,12 @@
 import { createApp } from 'vue'
 import { init } from '@telegram-apps/sdk-vue'
 import App from './App.vue'
+import { router } from './router'
+import './main.css'
 
 // Инициализируем SDK
 init()
 
 const app = createApp(App)
+app.use(router) // Добавляем роутер
 app.mount('#app')
