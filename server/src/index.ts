933f96e4c00d4e991480a08d2929641092dfeb38
litury <litury@icloud.com>
2024-11-01 23:54:29 +0500

    🔧 chore(сервер): Установлены корректные порты для запуска

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config'
 import { app } from './app'
 
-const PORT = Number(process.env.PORT) || 4001
+const PORT = Number(process.env.PORT) || 4000
 const HOST = process.env.HOST || '0.0.0.0'
 
 app.listen(PORT, HOST, () => {
